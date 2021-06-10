8d3f1b1d7a6be971b5d3a99a380e15f3f9520c30
sebireni <sebireni@gmail.com>
2021-06-10 12:22:25 +0200

    hamburgerday

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function _loadEvent() {
+
+  //const bodyElement = document.getElementById("body");  
+  //const buttonElement = document.getElementById("menu-btn");  
+
+  const elements = {
+    body: document.getElementById("body"),
+    button: document.getElementById("menu-btn"),
+    sections: document.querySelectorAll("section"),
+    menuUL:document.querySelector("#menu-content ul")
+  }
      
-function toggleMenu() {
- console.log("hello World!");
- document.getElementById("menu-content").classList.toggle("open");
-}
+  for (const section of elements.sections) {
+    //console.log(section.innerHTML)
+    elements.menuUL.insertAdjacentHTML("beforeend", `
+      <li><a href="#${section.getAttribute("id")}">${section.querySelector("h2").innerHTML}</a></li>
+    `)
+  }
 
-  const buttonElement = document.getElementById ("menu-btn");  
-    buttonElement.addEventListener("click" , toggleMenu);
+  function toggleMenu() {
+    elements.body.classList.toggle("menu-opened"); //global state - local affect
+  }
 
+  elements.button.addEventListener("click", toggleMenu);
 
 }
 
